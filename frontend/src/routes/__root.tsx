887361882350b1611087dc0902058f026cfbda11
vladwb <vladwithcode@gmail.com>
2025-10-29 20:02:07 -0600

    chore: remove tanstack dev tools

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,6 +1,4 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
-import { TanstackDevtools } from "@tanstack/react-devtools";
 import { Toaster } from "sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -11,17 +9,6 @@ export const Route = createRootRoute({
         <QueryClientProvider client={queryClient}>
             <Outlet />
             <Toaster />
-            <TanstackDevtools
-                config={{
-                    position: "bottom-left",
-                }}
-                plugins={[
-                    {
-                        name: "Tanstack Router",
-                        render: <TanStackRouterDevtoolsPanel />,
-                    },
-                ]}
-            />
         </QueryClientProvider>
     ),
 });
